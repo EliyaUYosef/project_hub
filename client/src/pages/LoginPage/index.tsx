df5d681fe0edc79f2a99d7cacd5a7d7089953629
Eliya.Y <Eliya.Yosef@gmail.com>
2023-09-27 18:46:36 +0300

    basic login form - todo : fix context provider

--- client/src/pages/LoginPage/index.tsx
+++ client/src/pages/LoginPage/index.tsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 interface LoginForm {
   email: string;
   password: string;
+}
+interface LoginErrors {
+  emailError?: string | "";
+  passwordError?: string | "";
 }
 
 const LoginPage: React.FC = () => {
@@ -10,11 +14,82 @@ const LoginPage: React.FC = () => {
     email: "",
     password: "",
   });
+  const [inputErrors, setInputErrors] = useState<LoginErrors>({
+    emailError: "",
+    passwordError: "",
+  });
+  const [loader, setLoader] = useState(false);
+  const [token, setToken] = useState("");
+
+  const [emailFlag, setEmailFlag] = useState(false);
+  const [passwordFlag, setPasswordFlag] = useState(false);
+  let apiError = false;
+  useEffect(() => {
+    let localToken = window.localStorage.getItem("token");
+    if (localToken !== null)
+      setToken(localToken ?? '');  
+  }, []);
+  useEffect(() => {
+    let savedData;
+    if (token !== '')
+      window.localStorage.setItem("token", token);
+  }, [token]);
 
   const handleInputChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ): void => {
     const { name, value } = event.target;
+
+    if (name === "email") {
+      if (value.length < 8) {
+        setInputErrors((prevErrors) => ({
+          ...prevErrors,
+          emailError: "Email must be at least 8 characters long",
+        }));
+        setEmailFlag(false);
+      } else if (!validateEmail(value)) {
+        setInputErrors((prevErrors) => ({
+          ...prevErrors,
+          emailError: "Invalid email format",
+        }));
+        setEmailFlag(false);
+      } else {
+        setEmailFlag(false);
+        if (value.length >= 8 && validateEmail(value)) {
+          setEmailFlag(true);
+        }
+        setInputErrors((prevErrors) => ({
+          ...prevErrors,
+          emailError: "",
+        }));
+      }
+    }
+    if (name === "password") {
+      let passwordValidation = !/[0-9]/.test(value) || !/[A-Z]/.test(value);
+      if (value.length < 10) {
+        setInputErrors((prevErrors) => ({
+          ...prevErrors,
+          passwordError: "Password must be at least 10 characters long",
+        }));
+        setPasswordFlag(false);
+      } else if (passwordValidation) {
+        setInputErrors((prevErrors) => ({
+          ...prevErrors,
+          passwordError:
+            "Password must contain at least one number and one capital letter",
+        }));
+        setPasswordFlag(false);
+      } else {
+        setPasswordFlag(false);
+        if (value.length >= 10 && !passwordValidation) {
+          setPasswordFlag(true);
+        }
+        setInputErrors((prevErrors) => ({
+          ...prevErrors,
+          passwordError: "",
+        }));
+      }
+    }
     setFormData((prevData) => ({
       ...prevData,
       [name]: value,
@@ -22,36 +97,41 @@ const LoginPage: React.FC = () => {
   };
 
   const handleSubmit = async (event: React.FormEvent): Promise<void> => {
+    setLoader(true);
     event.preventDefault();
 
-    // Perform input validation here (e.g., email format, password strength)
-
-    // Send a POST request to your authentication API
-    const response = await fetch("https://private-052d6-testapi4528.apiary-mock.com/authenticate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: formData.email,
-        password: formData.password,
-      }),
-    });
+    const response = await fetch(
+      "https://private-052d6-testapi4528.apiary-mock.com/authenticate",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: formData.email,
+          password: formData.password,
+        }),
+      }
+    );
 
     if (response.ok) {
       const data = await response.json();
-      // Store the authentication token in your application state or browser storage
-      // Redirect the user to the info page or handle the next steps as needed
-      console.log(data);
+
+      setToken(data[0].token);
+      setLoader(false);
     } else {
-      // Handle login failure (display error message, etc.)
+
       console.error("Login failed");
+      setLoader(false);
     }
   };
 
-  return (
+  return loader ? (
+    <div>Loading ... </div>
+  ) : (
     <div>
       <h1>Login Page</h1>
+      {apiError ? <div>Error</div> : <div></div>}
       <form onSubmit={handleSubmit}>
         <div>
           <label>Email:</label>
@@ -62,6 +142,7 @@ const LoginPage: React.FC = () => {
             onChange={handleInputChange}
             required
           />
+          <div className="email_error">{inputErrors.emailError}</div>
         </div>
         <div>
           <label>Password:</label>
@@ -73,10 +154,19 @@ const LoginPage: React.FC = () => {
             required
           />
         </div>
-        <button type="submit">Login</button>
+        <div className="email_error">{inputErrors.passwordError}</div>
+        <button
+          disabled={!(emailFlag && passwordFlag)}
+          type="submit"
+        >
+          Login
+        </button>
       </form>
     </div>
   );
 };
-
+function validateEmail(email: string) {
+  var regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(email);
+}
 export default LoginPage;
