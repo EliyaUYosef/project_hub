4199bf70d9c95a479261296ed0c5c93fdcd995d6
Eliya.Y <Eliya.Yosef@gmail.com>
2023-09-27 20:06:13 +0300

    added types file that contain all interfaces

---
+++ client/src/Types.ts
@@ -0,0 +1,37 @@
+
+export interface User {
+  id?: string | "";
+  name?: string | "";
+  score?: number | 0;
+  durationInDays?: number | 0;
+  bugsCount?: number | 0;
+  madeDadeline?: boolean | false;
+}
+
+export interface PersonalDetails {
+  Team: string | "";
+  avatar: string | "";
+  name: string | "";
+  joinedAt: string | "";
+}
+
+export interface GlobalData {
+  userData?: User | {};
+  setUserData?: React.Dispatch<React.SetStateAction<User>>;
+  apiToken?: string | '';
+  setApiToken?: React.Dispatch<React.SetStateAction<string>>;
+  personalDetails?: PersonalDetails | {};
+  setPersonalDetails?: React.Dispatch<React.SetStateAction<PersonalDetails>>;
+
+  setAppGlobalData: React.Dispatch<React.SetStateAction<GlobalData>>;
+}
+
+export interface LoginForm {
+  email: string;
+  password: string;
+}
+
+export interface LoginErrors {
+  emailError?: string | "";
+  passwordError?: string | "";
+}
